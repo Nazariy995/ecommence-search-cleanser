3806a76aeda0a0e158ffbc58e664252769d9e4c4
Nazariy <nazariy@trendala.com>
2015-04-30 14:40:26 -0400

    Working: Getting the price Not Working: Get merchant, or brand

--- cleanser.py
+++ cleanser.py
@@ -1,16 +1,31 @@
 import json
+import nltk
+import re
 
 class cleanser():
     def __init__(self, search_string):
-        self.search_string = search_string
+        self.search_string = self.clean_from_non_alphanumeric(search_string)
+        self.tokenized_string = self.tokenize_sentence()
         self.price = self.get_price()
-        self.brand = self.get_brand()
-        self.merchant = self.get_merchant()
+#        self.brand = self.get_brand()
+#        self.merchant = self.get_merchant()
+        print self.price
     
     #Cleaning the whole sentence from anything that is not a word
-    def clean_from_non_alphanumeric(self):
-        cleaned_string = re.sub(r'((?! )\W+)','',self.search_string)
-        self.search_string = cleaned_string
+    def clean_from_non_alphanumeric(self,search_string):
+        cleaned_string = re.sub(r'((?! )\W+)','',search_string)
+        return cleaned_string
         
-    def get_price():
-        price
+    #tokenize the search strng into its parts of speech
+    def tokenize_sentence(self):
+        tokenized_string = nltk.word_tokenize(self.search_string)
+        return nltk.pos_tag(tokenized_string)
+
+    def get_price(self):
+        prices = []
+        for index,word in enumerate(self.tokenized_string):
+            if word[1] == "CD":
+                prices.append(word[0])
+
+        return prices
+
